63534aeacf3ca6a67c8ab746497ea3af1edc3edd
Denis Bykov <dbykov965@gmail.com>
2026-05-20 12:57:23 +0300

    add tests and ci/cd pipelines

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,10 +1,9 @@
 from fastapi.testclient import TestClient
-
 from src.main import app
 
 client = TestClient(app)
 
-# Существующие пользователи
+# Существующие пользователи в фейковой БД
 users = [
     {
         'id': 1,
@@ -26,16 +25,32 @@ def test_get_existed_user():
 
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': 'nonexistent@mail.com'})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
-    pass
+    new_user_data = {
+        'name': 'Alex Smith',
+        'email': 'a.smith@example.com'
+    }
+    response = client.post("/api/v1/user", json=new_user_data)
+    assert response.status_code == 201
+    assert isinstance(response.json(), int)
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    duplicate_user_data = {
+        'name': 'Clone Petrov',
+        'email': users[1]['email']
+    }
+    response = client.post("/api/v1/user", json=duplicate_user_data)
+    assert response.status_code == 409
+    assert response.json() == {"detail": "User with this email already exists"}
 
 def test_delete_user():
     '''Удаление пользователя'''
-    pass
+    response = client.delete("/api/v1/user", params={'email': users[0]['email']})
+    assert response.status_code == 204
+    assert response.text == ""  
